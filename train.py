c23b6dfc5ef729acdf1716486841c7e42cb27579
charlielito <charlie@kiwicampus.com>
2019-03-16 18:41:33 -0500

    add stuff

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import sklearn
 import matplotlib.pyplot as plt
+import yaml
 
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
@@ -14,32 +15,85 @@ import tensorflow as tf
 
 from model import model, deep_model, nvidia_net
 
+tf.logging.set_verbosity(tf.logging.DEBUG)
 
-### Model Parameters
-conv_dropout = 0.4
-dense_dropout = 0.5
-l2_regularization = 0.001
+def load_params(filename):
 
-# l1_regularizer = tf.keras.regularizers.l1(0.01)
-l2_regularizer = None #tf.keras.regularizers.l2(l2_regularization)
+    with open(filename, 'r') as stream:
+        try:
+            return yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            return None
 
-CROP_UP = 50
-CROP_DOWN = 30
-final_height = 160 - (CROP_UP + CROP_DOWN)
+# Aux function to crop the images
+def get_crop_window(crop_up, crop_down):
+    final_height = 160 - (crop_up + crop_down)
+    final_width = 320
+
+    return [
+        crop_up,
+        0,
+        final_height,
+        final_width,
+    ]
 
 
-mu = 0
-sigma = 0.1
-init = None #tf.initializers.truncated_normal(mean = mu, stddev = sigma)
+# Function that returns the image decoded from jpg and label
+def parse_function(filename, label):
+    image_string = tf.read_file(filename)
 
-params = dict(
-    conv_dropout=conv_dropout,
-    dense_dropout=dense_dropout,
-    kernel_init=init,
-    height=final_height
-)
+    # Don't use tf.image.decode_image, or the output shape will be undefined
+    image = tf.image.decode_jpeg(image_string, channels=3)
+    
+    image = tf.image.decode_and_crop_jpeg(
+            image_string,
+            crop_window = get_crop_window(CROP_UP, CROP_DOWN),
+            channels = 3
+        )
 
-def cnn_model_fn(features, labels, mode):
+    image = tf.cast(image, tf.float32)
+    label = tf.cast(label, tf.float32)
+    return image, label
+
+
+def get_image_path(filename):
+    return os.path.join(DATASET,"IMG", filename.split("/")[-1])
+
+# Create training dataset
+def create_dataset(filenames, labels, EPOCHS, BATCH_SIZE):
+    dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
+    dataset = dataset.shuffle(len(filenames))
+    dataset = dataset.map(parse_function, num_parallel_calls=4)
+    dataset = dataset.repeat(EPOCHS)
+    dataset = dataset.batch(BATCH_SIZE)
+    dataset = dataset.prefetch(1)
+    return dataset
+
+
+# Serving function for exported model
+def serving_input_fn(params):
+
+    input_image = tf.placeholder(
+        dtype=tf.float32,
+        shape=[None, None, None, 3],
+        name="input_image"
+    )
+
+#     images = tf.image.resize_images(input_image, [params.image_height, params.image_width])
+    images = input_image
+    images = tf.image.crop_to_bounding_box(images, *get_crop_window(params["crop_up"], params["crop_down"]))
+
+    images = tf.cast(images, tf.float32)
+
+    return tf.estimator.export.TensorServingInputReceiver(
+        features = images,
+        receiver_tensors = input_image
+    )
+
+
+# Model function for estimator
+def cnn_model_fn(features, labels, mode, params):
     """Model function for CNN."""
 
     predictions = nvidia_net(features, mode, params)
@@ -89,35 +143,51 @@ def cnn_model_fn(features, labels, mode):
       mode=mode, loss=loss, eval_metric_ops={'mae/mae': mae}, evaluation_hooks=None)
 
 
+# Load parameters
+params = load_params("configs/train.yaml")
+
+### Model Parameters
+conv_dropout = params["conv_dropout"]
+dense_dropout = params["dense_dropout"]
+l2_regularization = params["l2_regularization"]
+
+# l1_regularizer = tf.keras.regularizers.l1(0.01)
+l2_regularizer = None #tf.keras.regularizers.l2(l2_regularization)
+
+CROP_UP = params["crop_up"]
+CROP_DOWN = params["crop_down"]
+final_height = 160 - (CROP_UP + CROP_DOWN)
+
+init = None #tf.initializers.truncated_normal(mean = mu, stddev = sigma)
+
+model_params = dict(
+    conv_dropout=conv_dropout,
+    dense_dropout=dense_dropout,
+    kernel_init=init,
+    height=final_height
+)
+
+BATCH_SIZE = params["batch_size"]
+EPOCHS = params["epochs"]
+
 # Create the Estimator
 classifier = tf.estimator.Estimator(
-    model_fn=cnn_model_fn, model_dir="/tmp/pilotnetVpaper6")
-
+    model_fn=cnn_model_fn, 
+    model_dir="experiments/{}".format(params["network"]),
+    params=model_params)
 
 # ## Dataset creation
-
 ### folder where the data is
-DATASET = "data_mine"
-
-
-
-def read_img(name):
-    img = name.split('/')[-1]
-    return cv2.imread(os.path.join(DATASET,"IMG", img))[...,::-1]
-
+DATASET = params["dataset"]
 
 
 ### Load csv as a pandas dataframe
 header = ["center", "left", "right", "steering", "throttle", "break", "speed"]
 df = pd.read_csv(os.path.join(DATASET, "driving_log.csv"), header=None, names=header)
-df.head()
+print(df.head())
 
 
-def get_image_path(filename):
-    return os.path.join(DATASET,"IMG", filename.split("/")[-1])
-
-
-### Make paths relative to this jupyter notebook
+### Make paths relative to this file
 df["center"] = df.center.apply(get_image_path)
 df["left"] = df.left.apply(get_image_path)
 df["right"] = df.right.apply(get_image_path)
@@ -136,7 +206,7 @@ val_labels = validation_samples.steering.values
 
 
 # Add side cameras to train set
-steer_correction = 0.2
+steer_correction = params["steer_correction"]
 
 l_filenames = train_samples.left.values
 l_labels = train_samples.steering.values + steer_correction
@@ -147,116 +217,34 @@ r_labels = train_samples.steering.values - steer_correction
 filenames = np.concatenate([c_filenames, l_filenames, r_filenames])
 labels = np.concatenate([c_labels, l_labels, r_labels])
 
-
-# Aux function to crop the images
-def get_crop_window(crop_up, crop_down):
-    final_height = 160 - (crop_up + crop_down)
-    final_width = 320
-
-    return [
-        crop_up,
-        0,
-        final_height,
-        final_width,
-    ]
-
-
-# Function that returns the image decoded from jpg and label
-def parse_function(filename, label):
-    image_string = tf.read_file(filename)
-
-    # Don't use tf.image.decode_image, or the output shape will be undefined
-    image = tf.image.decode_jpeg(image_string, channels=3)
-    
-    image = tf.image.decode_and_crop_jpeg(
-            image_string,
-            crop_window = get_crop_window(CROP_UP, CROP_DOWN),
-            channels = 3
-        )
-
-    image = tf.cast(image, tf.float32)
-    label = tf.cast(label, tf.float32)
-    return image, label
-
-
 # ### Use of TF DataSet API
-
 # Define parameters
-BATCH_SIZE = 32
-EPOCHS = 50
 steps_per_epoch = len(train_samples)*3//BATCH_SIZE
 
-
-# Create training dataset
-def create_train_dataset():
-    dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
-    dataset = dataset.shuffle(len(filenames))
-    dataset = dataset.map(parse_function, num_parallel_calls=4)
-    dataset = dataset.repeat(EPOCHS)
-    dataset = dataset.batch(BATCH_SIZE)
-    dataset = dataset.prefetch(1)
-    return dataset
-
-
-# Create dev dataset
-def create_eval_dataset():
-    val_dataset = tf.data.Dataset.from_tensor_slices((val_filenames, val_labels))
-    val_dataset = val_dataset.map(parse_function, num_parallel_calls=4)
-    val_dataset = val_dataset.repeat(EPOCHS)
-    val_dataset = val_dataset.batch(BATCH_SIZE)
-    val_dataset = val_dataset.prefetch(1)
-    return val_dataset
-
-
-# ## Training
-
-def serving_input_fn():
-
-    input_image = tf.placeholder(
-        dtype=tf.float32,
-        shape=[None, None, None, 3],
-        name="input_image"
-    )
-
-#     images = tf.image.resize_images(input_image, [params.image_height, params.image_width])
-    images = input_image
-    images = tf.image.crop_to_bounding_box(images, *get_crop_window(CROP_UP, CROP_DOWN))
-
-    images = tf.cast(images, tf.float32)
-
-    return tf.estimator.export.TensorServingInputReceiver(
-        features = images,
-        receiver_tensors = input_image
-    )
-
-
-
 exporter = tf.estimator.LatestExporter(
-    "test_exporter",
-    lambda: serving_input_fn(),
+    "exporter",
+    lambda: serving_input_fn(params),
 )
-
-
 
 # Specs
 train_spec = tf.estimator.TrainSpec(
-    input_fn=create_train_dataset,
+    input_fn=lambda: create_dataset(filenames, labels, EPOCHS, BATCH_SIZE),
     max_steps=EPOCHS*steps_per_epoch)
 
 eval_spec = tf.estimator.EvalSpec(
-    input_fn=create_eval_dataset,
+    input_fn=lambda: create_dataset(val_filenames, val_labels, EPOCHS, BATCH_SIZE),
     steps=None,
     exporters=[exporter],
     start_delay_secs=10,  # Start evaluating after 10 sec.
     throttle_secs=30  # Evaluate only every 30 sec
 )
 
-
+# Train loop
 tf.estimator.train_and_evaluate(classifier, train_spec, eval_spec)
 
-
+# Export
 classifier.export_savedmodel(
-    "export/nvidia_dataset",
-    lambda: serving_input_fn()
+    "export/{}".format(params["network"]),
+    lambda: serving_input_fn(params)
 )
 
--- writeup_template.md
+++ writeup_template.md
@@ -1,24 +1,15 @@
 # **Behavioral Cloning** 
+Carlos Andres Alvarez
+Self driving nanodegree Udacity
 
-## Writeup Template
-
-### You can use this file as a template for your writeup if you want to submit it as a markdown file, but feel free to use some other method and submit a pdf if you prefer.
+## Writeup
 
 ---
-
-**Behavioral Cloning Project**
-
-The goals / steps of this project are the following:
-* Use the simulator to collect data of good driving behavior
-* Build, a convolution neural network in Keras that predicts steering angles from images
-* Train and validate the model with a training and validation set
-* Test that the model successfully drives around track one without leaving the road
-* Summarize the results with a written report
 
 
 [//]: # (Image References)
 
-[image1]: ./examples/placeholder.png "Model Visualization"
+[image1]: ./results/nvidia_net.png "Model Visualization"
 [image2]: ./examples/placeholder.png "Grayscaling"
 [image3]: ./examples/placeholder_small.png "Recovery Image"
 [image4]: ./examples/placeholder_small.png "Recovery Image"
@@ -32,45 +23,72 @@ The goals / steps of this project are the following:
 ---
 ### Files Submitted & Code Quality
 
-#### 1. Submission includes all required files and can be used to run the simulator in autonomous mode
+#### 1. Files in project
 
 My project includes the following files:
-* model.py containing the script to create and train the model
-* drive.py for driving the car in autonomous mode
-* model.h5 containing a trained convolution neural network 
-* writeup_report.md or writeup_report.pdf summarizing the results
+* train.py containing the script to create and train the model
+* model.py that defines different conv architectures
+* data_augmentation.py for image data augmentation (actually dont used in the final version)
+* drive_estimator.py for driving the car in autonomous mode using the Estimator API from tensorflow
+* export/nvidia_dataset/1552541693/ containing a trained convolution neural network as a saved model
+* writeup_report.md summarizing the results
 
-#### 2. Submission includes functional code
-Using the Udacity provided simulator and my drive.py file, the car can be driven autonomously around the track by executing 
+#### 2. Functional code
+Using the Udacity provided simulator and my drive_estimator.py file, the car can be driven autonomously around the track by executing 
 ```sh
-python drive.py model.h5
+python drive.py export/nvidia_dataset/1552541693 22
 ```
+where 22 is the desired velocity set point.
 
-#### 3. Submission code is usable and readable
+#### 3. Usable and readable code
 
-The model.py file contains the code for training and saving the convolution neural network. The file shows the pipeline I used for training and validating the model, and it contains comments to explain how the code works.
+The train.py file contains the code for training and saving the convolution neural network. The file shows the pipeline I used for training and validating the model, and it contains comments to explain how the code works.
+
+I didn't use the keras API since tf.estimator shows a more flexible and configurable mode to define the network and run the training. In the model.py is the architecture defined.
 
 ### Model Architecture and Training Strategy
 
-#### 1. An appropriate model architecture has been employed
+#### 1. Model architecture
 
-My model consists of a convolution neural network with 3x3 filter sizes and depths between 32 and 128 (model.py lines 18-24) 
+My model consists of a convolution neural network with 5x5 and 3x3 filter sizes and depths between 36 and 64. See `nvidia_net` function in model.py line 416.The total architecture can be seen as follows:
 
-The model includes RELU layers to introduce nonlinearity (code line 20), and the data is normalized in the model using a Keras lambda layer (code line 18). 
+![alt text][image1]
+
+This network is based on Nvidia's paper [End to End Learning for Self-Driving Cars](https://arxiv.org/pdf/1604.07316.pdf). The only changes are the image input dimensions and the fully connected layers (number of neurons). All activations are RELU. I used also droput after each layer with a drop rate of 0.4 for convolutional layers and 0.5 for dense layers. Also batch normalization is applied to all layers including the input layer. 
 
 #### 2. Attempts to reduce overfitting in the model
 
-The model contains dropout layers in order to reduce overfitting (model.py lines 21). 
+The model contains dropout layers, as said, in order to reduce overfitting.
 
-The model was trained and validated on different data sets to ensure that the model was not overfitting (code line 10-16). The model was tested by running it through the simulator and ensuring that the vehicle could stay on the track.
+The model was trained and validated on different data sets to ensure that the model was not overfitting. Here I create 2 tf.estimator Specs for evaluating in different datasets. The split was made 80-20 from the dataset.
+
+```
+# Specs
+train_spec = tf.estimator.TrainSpec(
+    input_fn=lambda: create_dataset(filenames, labels, EPOCHS, BATCH_SIZE),
+    max_steps=EPOCHS*steps_per_epoch)
+
+eval_spec = tf.estimator.EvalSpec(
+    input_fn=lambda: create_dataset(val_filenames, val_labels, EPOCHS, BATCH_SIZE),
+    steps=None,
+    exporters=[exporter],
+    start_delay_secs=10,  # Start evaluating after 10 sec.
+    throttle_secs=30  # Evaluate only every 30 sec
+)
+
+# Train loop
+tf.estimator.train_and_evaluate(classifier, train_spec, eval_spec)
+```
+
+The model was tested by running it through the simulator and ensuring that the vehicle could stay on the track.
 
 #### 3. Model parameter tuning
 
-The model used an adam optimizer, so the learning rate was not tuned manually (model.py line 25).
+The model used an adam optimizer, so the learning rate was not tuned manually (train.py line 129). I used a learning rate of 0.001.
 
 #### 4. Appropriate training data
 
-Training data was chosen to keep the vehicle driving on the road. I used a combination of center lane driving, recovering from the left and right sides of the road ... 
+Training data was chosen to keep the vehicle driving on the road. I used only center driving and in traning time the model used also right and left camera information.
 
 For details about how I created the training data, see the next section. 
 
